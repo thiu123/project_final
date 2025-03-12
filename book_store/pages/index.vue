f5100b8369019f18ee76583a06405a8760b72f79
Bùi Trung Hiếu <134358078+thiu123@users.noreply.github.com>
2025-03-12 08:36:30 +0700

    update home page book, logic for take book in subjects

--- book_store/pages/index.vue
+++ book_store/pages/index.vue
@@ -149,69 +149,9 @@
         </v-tabs-window>
       </v-card>
 
-      <!-- New Arrivals -->
-      <v-container class="mt-8">
-        <div class="d-flex justify-space-between align-center mb-4">
-          <h2 class="text-h5 font-weight-bold">New Arrivals</h2>
-        </div>
-        <v-row>
-          <v-col v-for="(book, i) in books" :key="i" cols="6" sm="4" md="2">
-            <v-card class="h-100 bg-transparent" elevation="2">
-              <div class="position-relative">
-                <v-img
-                  class=""
-                  :src="book.cover_url"
-                  height="250"
-                  cover
-                ></v-img>
-                <v-btn
-                  icon
-                  variant="text"
-                  color="white"
-                  class="position-absolute"
-                  style="top: 8px; right: 8px"
-                >
-                  <v-icon>mdi-heart</v-icon>
-                </v-btn>
-              </div>
-              <v-card-text class="pa-2">
-                <div class="d-flex align-center mb-1">
-                  <v-rating
-                    :model-value="book.rating"
-                    color="amber"
-                    density="compact"
-                    size="small"
-                    readonly
-                  ></v-rating>
-                  <span class="text-caption ml-1">{{ book.reviews }}</span>
-                </div>
-                <div class="text-subtitle-2 font-weight-bold text-truncate">
-                  {{ book.title }}
-                </div>
-                <div class="d-flex justify-space-between align-center mt-2">
-                  <div>
-                    <span class="text-subtitle-2 font-weight-bold ml-1"
-                      >{{ book.price }} $</span
-                    >
-                  </div>
-                </div>
-              </v-card-text>
-              <v-card-actions>
-                <v-btn
-                  block
-                  color="white"
-                  class="bg-darkgreen rounded-xl"
-                  size="small"
-                >
-                  Add To Cart
-                  <v-icon class="ml-1">mdi-cart</v-icon>
-                </v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-container>
-
+      <BookFiction />
+      <BookManga />
+      <BookRomance />
       <!-- Best Selling Books -->
       <v-container class="mt-8">
         <v-card elevation="2" color="grey-lighten-4" class="pa-4 rounded-xl">
@@ -560,6 +500,7 @@
 <script>
 import { mapState, mapActions, mapGetters } from "vuex";
 import axios from "axios";
+import debounce from "lodash/debounce";
 export default {
   name: "Home",
   data() {
@@ -621,8 +562,8 @@ export default {
     };
   },
   watch: {
-    async searchQuery(newQuery) {
-      if (!newQuery) {
+    searchQuery: debounce(async function (newQuery) {
+      if (!newQuery.trim()) {
         this.searchResults = [];
         return;
       }
@@ -634,24 +575,33 @@ export default {
             params: { title: newQuery },
           }
         );
+
         const apiResults = response.data.docs || [];
-        this.searchResults = apiResults.filter((book) =>
-          this.getTitleBooks.some((storedBook) => storedBook.title === book.title)
+
+        const validBooks = apiResults.filter((book) =>
+          this.getTitleBooks.some(
+            (storedBook) =>
+              storedBook.title.toLowerCase() === book.title?.toLowerCase() &&
+              storedBook.first_publish_year === book.first_publish_year
+          )
         );
+
+        this.searchResults = validBooks.filter((book) =>
+          book.title.toLowerCase().includes(newQuery.toLowerCase())
+        );
+
         console.log("Search Results:", this.searchResults);
       } catch (error) {
-        console.error(error);
+        console.error("Lỗi khi tìm kiếm:", error);
       }
-    },
+    }, 300),
   },
   computed: {
     ...mapGetters("book", ["getTitleBooks"]),
     ...mapState("book", ["books"]),
   },
-  async mounted() {
-    await this.getAllBooks("fiction");
-    console.log("Books:", this.books);
-    console.log(this.getTitleBooks);
+  mounted() {
+    this.getAllBooks();
   },
   methods: {
     ...mapActions("book", ["getAllBooks"]),
